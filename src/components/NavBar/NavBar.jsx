e9432800f0723c19be974cda95d4cc1797dd2263
sandeep <sandyk1536@gmail.com>
2024-01-22 15:54:47 +0530

    latest

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -4,10 +4,14 @@ import HeaderLogo from "../../assets/header-logo.png";
 import styles from "./NavBar.module.css";
 import Search from "../SearchBar/Search.jsx";
 
+
 const NavBar = () => {
   return (
     <nav className={styles.navbar}>
-      <img src={HeaderLogo} alt="headerLogo"/>
+      <div>
+      <img src={HeaderLogo} alt="headerLogo" className={styles.logo}/>
+      </div>
+      
       <Search placeholder="Search an album of your choice" />
       <Button children="Give Feedback" />
     </nav>
