73a3fd78090d3a71056a187a5adccaaf875ce663
sandeep <sandyk1536@gmail.com>
2024-01-22 17:40:26 +0530

    heroN

--- src/components/HeroSection/HeroSection.jsx
+++ src/components/HeroSection/HeroSection.jsx
@@ -7,21 +7,23 @@ const HeroSection = () => {
   return (
     <div className={styles.container} data-testid="heroSection">
       <div className={styles.textContainer}>
-        <div className={styles.firstText} data-testid="firstText">
-          100 Thousand Songs, ad-free
+        <div className={styles.text}>
+          <div className={styles.firstText} data-testid="firstText">
+            100 Thousand Songs, ad-free
+          </div>
+          <img
+            src={Headphone}
+            alt="Headphone"
+            className={styles.heroImage}
+            data-testid="heroImage"
+            width={50} 
+            height={50} 
+          />
         </div>
         <div className={styles.secondText} data-testid="secondText">
           Over thousands podcast episodes
         </div>
       </div>
-      <img
-        src={Headphone}
-        alt="Headphone"
-        className={styles.heroImage}
-        data-testid="heroImage"
-        width={212}
-        height={212}
-      />
     </div>
   );
 };
