ae5668cdee61fc25a13bccff7788e1dda5ca4399
sandeep <sandyk1536@gmail.com>
2024-01-22 23:53:40 +0530

    latestherosection

--- src/components/HeroSection/HeroSection.jsx
+++ src/components/HeroSection/HeroSection.jsx
@@ -1,23 +1,20 @@
 import React from "react";
-import styles from "./HeroSection.module.css";
+import styles from "../HeroSection/HeroSection.module.css"
 import Headphone from "../../assets/headphone_logo.png";
 
-const HeroSection = () => {
-	return (
-		<>
-			<div className={styles.wrapper}>
-				<div className={styles.heroImageWrapper}>
-					<div className={styles.banner}>
-						<h1>100 Thousand Songs, ad free</h1>
-						<h1>Over thousands podcast episodes</h1>
-					</div>
-					<div>
-						<img src={Headphone} alt="Headphone" height="212px" width="212px" />
-					</div>
-				</div>
-			</div>
-		</>
-	);
-};
+function HeroSection(){
+  return (
+    <div className={styles.hero}>
+      <div>
+        <h1>100 Thousand Songs, ad-free</h1>
+        <h1>Over thousand podcast episodes</h1>
+      </div>
+      <div>
+        <img src ={Headphone} width = {212} alt = "headphones"/>
+      </div>
+    </div>
+  )
+} 
+
 
 export default HeroSection;
